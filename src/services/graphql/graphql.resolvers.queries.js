14356c3d2f4243e5a1bc269b03b4ce25dbf2ba16
Pankaj <pankajjatav7777@gmail.com>
2018-10-28 00:55:36 +0530

    Added functionality to use limit and skip as a parameters to fetch data through /graphql endpoint

--- src/services/graphql/graphql.resolvers.queries.js
+++ src/services/graphql/graphql.resolvers.queries.js
@@ -6,6 +6,12 @@ export default function(Query, Service, GetServiceName, FindServiceName) {
   });
   Object.assign(Query, {
     [`${FindServiceName}`]: (root, args, context) => {
+      if(typeof args.skip !== 'undefined') {
+        delete Object.assign(args, {['$skip']: args['skip'] })['skip'];
+      }
+      if(typeof args.limit !== 'undefined') {
+        delete Object.assign(args, {['$limit']: args['limit'] })['limit'];
+      }
       return Service.find(Object.assign({}, context, { query: args })).then(result => {
         return { total: result.length, items: result };
       });
